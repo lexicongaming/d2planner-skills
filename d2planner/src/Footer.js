a48d3a4afbab5190c2bdb4b98da3f559bbfe9cab
Nakroma <tarackobarack@gmail.com>
2022-12-19 12:34:58 +0100

    Modified styling and footer

--- d2planner/src/Footer.js
+++ d2planner/src/Footer.js
@@ -1,24 +1,15 @@
 import './Footer.css';
 
 function Footer (props) {
-  function sendEmail () {
-    window.open("mailto:d2plannerissues@gmail.com?subject=Report%20Issue");
-  }
-
   return (
     <div className='footerContainer'>
       <hr/>
       <div className='footerContentContainer'>
         <div className='footerInfoContainer'>
-          <p className='openSource'>An open source project (<a href='https://github.com/d2planner/skills'>view</a>).</p>
+          <p className='openSource'>Made by the amazing people at d2planner (<a href='https://github.com/d2planner/skills'>view</a>).</p>
+          <p className='openSource'>Any modified code is available <a href='https://github.com/lexicongaming/d2planner-skills'>here</a>.</p>
           <p className='copyRight'>© Copyright 2021, D2 Planner Developers</p>
         </div>
-        <button
-          className='reportIssue'
-          onClick={sendEmail}
-        >
-        Report Issue
-        </button>
       </div>
     </div>
   )
